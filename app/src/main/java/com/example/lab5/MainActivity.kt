a33e85eb62267f7f51177d80e298126502be6087
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-05-26 00:30:19 +0400

    fix: add flight card

--- app/src/main/java/com/example/lab5/MainActivity.kt
+++ app/src/main/java/com/example/lab5/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -26,12 +30,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.traversalIndex
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.room.Room
+import com.example.lab5.data.Airport
 import com.example.lab5.data.AppDatabase
 import com.example.lab5.ui.theme.Lab5Theme
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -88,10 +93,59 @@ fun FlightSearchBar(modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun Flight(
+    departureAirport: Airport,
+    arrivalAirport: Airport,
+    onClick: () -> Unit,
+    isClicked: Boolean,
+    modifier: Modifier = Modifier
+) {
+    Card(modifier = modifier) {
+        Column(modifier = Modifier.padding(8.dp)) {
+            Text("DEPART")
+            Row() {
+                Text(departureAirport.iataCode)
+                Text(departureAirport.name)
+            }
+            Text("ARRIVE")
+            Row {
+                Text(arrivalAirport.iataCode)
+                Text(arrivalAirport.name)
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Lab5Theme {
-        Text("123")
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = {
+                FlightSearchBar(modifier = Modifier.fillMaxWidth())
+            }
+        ) { innerPadding ->
+            Column (modifier = Modifier.padding(innerPadding).padding(12.dp).fillMaxWidth()) {
+                Flight(
+                    departureAirport = Airport(
+                        name = "aboba",
+                        id = 1,
+                        iataCode = "ABC",
+                        passengers = 1,
+                    ),
+                    arrivalAirport = Airport(
+                        name = "bob",
+                        id = 2,
+                        iataCode = "XYZ",
+                        passengers = 2,
+                    ),
+                    onClick = {},
+                    isClicked = false,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
     }
 }
